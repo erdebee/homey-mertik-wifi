7822c6dd8bd26915dfc88193b35055da9201be4b
petero-dk <2478689+petero-dk@users.noreply.github.com>
2024-05-31 14:23:35 +0200

    add debug files

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ class MertikWifiApp extends Homey.App {
    * onInit is called when the app is initialized.
    */
   async onInit() {
+    if (process.env.DEBUG === '1'){
+      try{ 
+        require('inspector').waitForDebugger();
+      }
+      catch(error){
+        require('inspector').open(9225, '0.0.0.0', true);
+      }
+  }
+  
     this.log('MertikWifiApp has been initialized');
   }
 }
